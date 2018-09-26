f139f8bfe49c0052f8cb934b522d69f4cb74726b
Daniel King <daniel@allenai.org>
2018-09-26 14:55:21 -0700

    Add pylint disables

--- SciSpaCy/util.py
+++ SciSpaCy/util.py
@@ -1,7 +1,7 @@
 import spacy
 
-from custom_sentence_segmenter import combined_rule_sentence_segmenter
-from custom_tokenizer import combined_rule_tokenizer
+from custom_sentence_segmenter import combined_rule_sentence_segmenter # pylint: disable-msg=E0611,E0401
+from custom_tokenizer import combined_rule_tokenizer # pylint: disable-msg=E0611,E0401
 
 def save_model(nlp, outputPath):
     nlp.to_disk(outputPath)
