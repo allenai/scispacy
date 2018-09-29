14c6f00fb56d72e5b7429f1947536a111ab61601
Daniel King <daniel@allenai.org>
2018-09-28 18:48:22 -0700

    Merge paren and bracket code

--- SciSpaCy/custom_sentence_segmenter.py
+++ SciSpaCy/custom_sentence_segmenter.py
@@ -8,8 +8,10 @@ def combined_rule_sentence_segmenter(doc):
     prev_tokens = [None, None]
 
     # keep stacks for determining when we are inside parenthesis or brackets
-    open_parens = []
-    open_brackets = []
+    parens_stack = []
+    brackets_stack = []
+    open_paren_to_stack = {"(": parens_stack, "[": brackets_stack}
+    close_paren_to_stack = {")": parens_stack, "]": brackets_stack}
     for token in doc:
         # handling special quote symbols
         # for example: 'in word order or syntactic structure (e.g., “cats climb trees” vs. “trees climb cats”).'
@@ -47,26 +49,38 @@ def combined_rule_sentence_segmenter(doc):
 
         # sentences can only start with ( if there is a complete sentence within the parens
         # here a . is serving as a proxy for being a complete sentence
-        # This isn't quite correct, as a sentence may start with (Bottom left) and this prevents that
-        # from being segmented correctly
-        if token.text == "(":
-            open_parens.append(token)
-        if token.text == ")" and open_parens != []:
-            last_open_paren = open_parens.pop()
+        # This isn't quite correct, as a sentence may start with (Bottom left) or [Bottom left] 
+        # and this prevents that from being segmented correctly
+        open_parens = ["(", "["]
+        close_parens = [")", "]"]
+        if token.text in open_parens:
+            open_paren_to_stack[token.text].append(token)
+        if token.text in close_parens and close_paren_to_stack[token.text] != []:
+            last_open_paren = close_paren_to_stack[token.text].pop()
             if prev_tokens[-1] and prev_tokens[-1].text != ".":
-                # allow things like (A) to start a sentence
+                # allow things like (A) or [A] to start a sentence
                 if not (last_open_paren.i == (token.i-2) and len(prev_tokens[-1].text) == 1):
                     doc[last_open_paren.i].is_sent_start = False
 
-        # same logic as above but for brackets instead of parens
-        if token.text == "[":
-            open_brackets.append(token)
-        if token.text == "]" and open_brackets != []:
-            last_open_bracket = open_brackets.pop()
-            if prev_tokens[-1] and prev_tokens[-1].text != ".":
-                # allow things like [A] to start a sentence
-                if not (last_open_bracket.i == (token.i-2) and len(prev_tokens[-1].text) == 1):
-                    doc[last_open_bracket.i].is_sent_start = False
+
+        # if token.text == "(":
+        #     open_parens.append(token)
+        # if token.text == ")" and open_parens != []:
+        #     last_open_paren = open_parens.pop()
+        #     if prev_tokens[-1] and prev_tokens[-1].text != ".":
+        #         # allow things like (A) to start a sentence
+        #         if not (last_open_paren.i == (token.i-2) and len(prev_tokens[-1].text) == 1):
+        #             doc[last_open_paren.i].is_sent_start = False
+
+        # # same logic as above but for brackets instead of parens
+        # if token.text == "[":
+        #     open_brackets.append(token)
+        # if token.text == "]" and open_brackets != []:
+        #     last_open_bracket = open_brackets.pop()
+        #     if prev_tokens[-1] and prev_tokens[-1].text != ".":
+        #         # allow things like [A] to start a sentence
+        #         if not (last_open_bracket.i == (token.i-2) and len(prev_tokens[-1].text) == 1):
+        #             doc[last_open_bracket.i].is_sent_start = False
 
         # handling the case of a capital letter after a ) unless that was preceeded by a .
         # for example: 'the support of the Defense Advanced Resarch Projects Agency (DARPA) Deep Exploration'
@@ -91,11 +105,11 @@ def combined_rule_sentence_segmenter(doc):
         prev_tokens = prev_tokens[1:] + [token]
 
     # any unmatched parens should not be the start of a sentence
-    for open_paren in open_parens:
+    for open_paren in parens_stack:
         doc[open_paren.i].is_sent_start = False
 
     # any unmatched brackets should not be the start of a sentence
-    for open_bracket in open_brackets:
+    for open_bracket in brackets_stack:
         doc[open_bracket.i].is_sent_start = False
 
     return doc
