e9b5e25574bef0c895e9613f4a76adfcea277902
danielkingai2 <daniel@allenai.org>
2018-12-04 14:11:18 -0800

    Add scripts to count sentences in a conll file, needed for spacy convert/train

---
+++ scripts/count_conll_sentences.py
@@ -0,0 +1,23 @@
+import argparse
+
+def main(conll_path):
+    with open(conll_path) as fp:
+        line = fp.readline()
+        sents = 0
+        while line:
+            if line.startswith("\n"):
+                sents += 1
+            line = fp.readline()
+            print(repr(line))
+    print("{} sentences in {}".format(sents, conll_path))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--conll_path',
+        help="Path to the conll data to count the sentences of"
+    )
+
+    args = parser.parse_args()
+    main(args.conll_path)
