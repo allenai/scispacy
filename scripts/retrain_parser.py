920177ed0297cc2f5e46f488c29c8b256bfd74fb
danielkingai2 <daniel@allenai.org>
2018-12-07 13:12:00 -0800

    add proper arguments to parser script

--- scripts/retrain_parser.py
+++ scripts/retrain_parser.py
@@ -6,25 +6,33 @@ import random
 from spacy import util
 from timeit import default_timer as timer
 from spacy.cli.train import print_progress
+from spacy.vocab import vocab
 import argparse
 import json
 import spacy_convert
 
-def train_parser(train_json_path: str,
-                 dev_json_path: str,
-                 test_json_path: str,
-                 output_dir: str):
+def train_parser(train_conll_path: str,
+                 dev_conll_path: str,
+                 test_conll_path: str,
+                 train_pmids_path: str,
+                 dev_pmids_path: str,
+                 test_pmids_path: str,
+                 vocab_path: str,
+                 model_output_dir: str):
     """Function to train the spacy parser from a blank model, with the default, en_core_web_sm vocab.
        Training setup is mostly copied from the spacy cli train command.
 
-       @param train_json_path: path to the training data in spacy json format
-       @param dev_json_path: path to the development data in spacy json format
-       @param test_json_path: path to the test data in spacy json format
-       @param output_dir: path to the directory to output the trained models to
+       @param train_conll_path: path to the conll formatted training data
+       @param dev_conll_path: path to the conll formatted dev data
+       @param test_conll_path: path to the conll formatted test data
+       @param train_pmids_path: path to the training pmids, one per conll sentence
+       @param dev_pmids_path: path to the dev pmids, one per conll sentence
+       @param test_pmids_path: path to the test pmids, one per conll sentence
+       @param vocab_path: path to the vocab to load
+       @param model_output_dir: path to the output directory for the trained models
     """
-    nlp_en = spacy.load('en_core_web_sm')
     nlp = spacy.blank('en')
-    nlp.vocab = nlp_en.vocab
+    nlp.vocab = Vocab().from_disk(vocab_path)
 
     if 'parser' not in nlp.pipe_names:
         parser = nlp.create_pipe('parser')
@@ -32,9 +40,9 @@ def train_parser(train_json_path: str,
     else:
         parser = nlp.get_pipe('parser')
 
-    train_corpus = spacy_convert.convert_abstracts_to_docs("../data/daniel_data/train.conllu", "../data/daniel_data/train_new.pmids", "../SciSpaCy/models/combined_all_model/vocab")
-    dev_corpus = spacy_convert.convert_abstracts_to_docs("../data/daniel_data/dev.conllu", "../data/daniel_data/dev_new.pmids", "../SciSpaCy/models/combined_all_model/vocab")
-    test_corpus = spacy_convert.convert_abstracts_to_docs("../data/daniel_data/test.conllu", "../data/daniel_data/test_new.pmids", "../SciSpaCy/models/combined_all_model/vocab")
+    train_corpus = spacy_convert.convert_abstracts_to_docs(train_conll_path, train_pmids_path, vocab_path)
+    dev_corpus = spacy_convert.convert_abstracts_to_docs(dev_conll_path, dev_pmids_path, vocab_path)
+    test_corpus = spacy_convert.convert_abstracts_to_docs(test_conll_path, test_pmids_path, vocab_path)
     n_train_words = sum(len(doc_gold[0]) for doc_gold in train_corpus)
 
     dropout_rates = util.decaying(util.env_opt('dropout_from', 0.2),
@@ -63,7 +71,7 @@ def train_parser(train_json_path: str,
 
         # save intermediate model and output results on the dev set
         with nlp.use_params(optimizer.averages):
-            epoch_model_path = '../SciSpaCy/models/manual-model'+str(i)
+            epoch_model_path = os.path.join(model_output_dir, "model"+str(i))
             nlp.to_disk(epoch_model_path)
             nlp_loaded = util.load_model_from_path(epoch_model_path)
             nwords = sum(len(doc_gold[0]) for doc_gold in dev_corpus)
@@ -91,8 +99,8 @@ def train_parser(train_json_path: str,
 
     # save final model and output results on the test set
     with nlp.use_params(optimizer.averages):
-        nlp.to_disk(os.path.join("../SciSpaCy/models/", "genia_trained_parser"))
-    nlp_loaded = util.load_model_from_path(os.path.join("../SciSpaCy/models/", "genia_trained_parser"))
+        nlp.to_disk(os.path.join(model_output_dir, "genia_trained_parser"))
+    nlp_loaded = util.load_model_from_path(os.path.join(model_output_dir, "genia_trained_parser"))
     nwords = sum(len(doc_gold[0]) for doc_gold in test_corpus)
     start_time = timer()
     scorer = nlp_loaded.evaluate(test_corpus)
@@ -103,41 +111,76 @@ def train_parser(train_json_path: str,
     print("Test results:")
     print("UAS:", scorer.unlabelled.fscore)
     print("LAS:", scorer.labelled.fscore)
-    with open(os.path.join(output_dir, "genia_trained_parser", "meta.json"), "w") as meta_fp:
+    with open(os.path.join(model_output_dir, "genia_trained_parser", "meta.json"), "w") as meta_fp:
         meta_fp.write(json.dumps(meta))
 
-def main(train_json_path,
-         dev_json_path,
-         test_json_path,
-         output_dir):
+def main(train_conll_path,
+         dev_conll_path,
+         test_conll_path,
+         train_pmids_path,
+         dev_pmids_path,
+         test_pmids_path,
+         vocab_path,
+         model_output_dir):
 
-    train_parser(train_json_path, dev_json_path, test_json_path, output_dir)
+    train_parser(train_conll_path,
+                 dev_conll_path,
+                 test_conll_path,
+                 train_pmids_path,
+                 dev_pmids_path,
+                 test_pmids_path,
+                 vocab_path,
+                 model_output_dir)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        '--train_json_path',
-        help="Path to the json formatted training data"
+        '--train_conll_path',
+        help="Path to the conll formatted training data"
     )
 
     parser.add_argument(
-        '--dev_json_path',
-        help="Path to the json formatted dev data"
+        '--dev_conll_path',
+        help="Path to the conll formatted dev data"
     )
 
     parser.add_argument(
-        '--test_json_path',
-        help="Path to the json formatted test data"
+        '--test_conll_path',
+        help="Path to the conll formatted test data"
     )
 
     parser.add_argument(
-        '--output_dir',
+        '--train_pmids_path',
+        help="Path to the training pmids, one per conll sentence"
+    )
+
+    parser.add_argument(
+        '--dev_pmids_path',
+        help="Path to the dev pmids, one per conll sentence"
+    )
+
+    parser.add_argument(
+        '--test_pmids_path',
+        help="Path to the test pmids, one per conll sentence"
+    )
+
+    parser.add_argument(
+        '--vocab_path',
+        help="Path to the spacy vocabulary to load"
+    )
+
+    parser.add_argument(
+        '--model_output_dir',
         help="Path to the directory to output the trained models to"
     )
 
     args = parser.parse_args()
-    main(args.train_json_path,
-         args.dev_json_path,
-         args.test_json_path,
-         args.output_dir)
+    main(args.train_conll_path,
+         args.dev_conll_path,
+         args.test_conll_path,
+         args.train_pmids_path,
+         args.dev_pmids_path,
+         args.test_pmids_path,
+         args.vocab_path,
+         args.model_output_dir)
