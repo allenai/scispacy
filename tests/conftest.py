a66815c50d3ca864a7d48e549eda4b1a43b737eb
Mark Neumann <markng@allenai.org>
2018-12-10 13:27:04 -0800

    check global state before test

--- tests/conftest.py
+++ tests/conftest.py
@@ -84,9 +84,11 @@ def test_vocab_dir():
 
 @pytest.fixture()
 def combined_all_model_fixture():
-    SpacyModelType.factories['combined_rule_sentence_segmenter'] = lambda nlp, **cfg: combined_rule_sentence_segmenter # pylint: disable=line-too-long
+    if SpacyModelType.factories.get('combined_rule_sentence_segmenter', None) is None:
+        SpacyModelType.factories['combined_rule_sentence_segmenter'] = lambda nlp, **cfg: combined_rule_sentence_segmenter # pylint: disable=line-too-long
     nlp = get_spacy_model('SciSpaCy/models/combined_all_model', True, True, True,
-                          with_custom_tokenizer=True)
+                          with_custom_tokenizer=True,
+                          with_sentence_segmenter=False)
     return nlp
 
 @pytest.fixture()
