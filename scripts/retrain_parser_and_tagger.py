b66a0f5dbb015d8b1b3160cae9cedd66cfa925cb
danielkingai2 <daniel@allenai.org>
2018-12-13 19:45:21 -0800

    refactor training script to read json format and use GoldCorpus

--- scripts/retrain_parser_and_tagger.py
+++ scripts/retrain_parser_and_tagger.py
@@ -14,30 +14,24 @@ import json
 import spacy_convert
 import random
 
-def train_parser_and_tagger(train_conll_path: str,
-                            dev_conll_path: str,
-                            test_conll_path: str,
-                            train_pmids_path: str,
-                            dev_pmids_path: str,
-                            test_pmids_path: str,
+def train_parser_and_tagger(train_json_path: str,
+                            dev_json_path: str,
+                            test_json_path: str,
                             vocab_path: str,
                             model_output_dir: str,
                             ontonotes_path: str = None):
     """Function to train the spacy parser and tagger from a blank model, with the default, en_core_web_sm vocab.
        Training setup is mostly copied from the spacy cli train command.
 
-       @param train_conll_path: path to the conll formatted training data
-       @param dev_conll_path: path to the conll formatted dev data
-       @param test_conll_path: path to the conll formatted test data
-       @param train_pmids_path: path to the training pmids, one per conll sentence
-       @param dev_pmids_path: path to the dev pmids, one per conll sentence
-       @param test_pmids_path: path to the test pmids, one per conll sentence
+       @param train_json_path: path to the conll formatted training data
+       @param dev_json_path: path to the conll formatted dev data
+       @param test_json_path: path to the conll formatted test data
        @param vocab_path: path to the vocab to load
        @param model_output_dir: path to the output directory for the trained models
        @param ontonotes_path: path to the directory containnig ontonotes in spacy format (optional)
     """
-    nlp = spacy.blank('en')
-    nlp.vocab = Vocab().from_disk(vocab_path)
+    lang_class = util.get_lang_class('en')
+    nlp = lang_class()
 
     if 'tagger' not in nlp.pipe_names:
         tagger = nlp.create_pipe('tagger')
@@ -51,16 +45,8 @@ def train_parser_and_tagger(train_conll_path: str,
     else:
         parser = nlp.get_pipe('parser')
 
-    train_corpus = spacy_convert.convert_abstracts_to_docs(train_conll_path, train_pmids_path, vocab_path)
-    dev_corpus = spacy_convert.convert_abstracts_to_docs(dev_conll_path, dev_pmids_path, vocab_path)
-    test_corpus = spacy_convert.convert_abstracts_to_docs(test_conll_path, test_pmids_path, vocab_path)
-    n_train_words = sum(len(doc_gold[0]) for doc_gold in train_corpus)
-
-    nlp = spacy.load("SciSpaCy/models/cl/model0")
-    test_corpus = spacy_convert.convert_abstracts_to_docs(test_conll_path, test_pmids_path, vocab_path)
-    scorer = nlp.evaluate(test_corpus)
-    print(scorer.uas, scorer.las, scorer.tags_acc)
-    asdf
+    train_corpus = GoldCorpus(train_json_path, dev_json_path)
+    test_corpus = GoldCorpus(train_json_path, dev_json_path)
 
     if ontonotes_path:
         onto_train_path = os.path.join(ontonotes_path, "train")
@@ -85,27 +71,21 @@ def train_parser_and_tagger(train_conll_path: str,
     meta["parent_package"] = "spacy"
     meta["email"] = "ai2-info@allenai.org"
 
-    # this is the set of tags that is in spacy's english pos scheme but not in GENIA train
-    # {'GW', 'XX', '_SP', 'UH', '#', '""', 'HYPH', 'NFP', '$', 'BES', 'HVS', 'NIL', 'ADD', 'AFX'}
-    for doc, gold in train_corpus:
-        for label in gold.labels:
-            parser.add_label(label)
-
-        for tag in gold.tags:
-            tagger.add_label(tag)
+    n_train_words = train_corpus.count_train()
 
     other_pipes = [pipe for pipe in nlp.pipe_names if pipe not in  ['tagger', 'parser']]
 
-    optimizer = nlp.begin_training()
+    optimizer = nlp.begin_training(lambda: train_corpus.train_tuples)
     nlp._optimizer = None
     print("Itn.  Dep Loss  NER Loss  UAS     NER P.  NER R.  NER F.  Tag %   Token %  CPU WPS  GPU WPS")
     with nlp.disable_pipes(*other_pipes):
+        train_docs = train_corpus.train_docs(nlp, projectivize=True)
+        train_docs = list(train_docs)
         for i in range(10):
-            train_corpus = spacy_convert.convert_abstracts_to_docs(train_conll_path, train_pmids_path, vocab_path)
-            random.shuffle(train_corpus)
+            random.shuffle(train_docs)
             with tqdm(total=n_train_words, leave=False) as pbar:
                 losses = {}
-                minibatches = list(util.minibatch(train_corpus, size=batch_sizes))
+                minibatches = list(util.minibatch(train_docs, size=batch_sizes))
                 for batch in minibatches:
                     docs, golds = zip(*batch)
                     nlp.update(docs, golds, sgd=optimizer,
@@ -121,11 +101,12 @@ def train_parser_and_tagger(train_conll_path: str,
                     meta["version"] = str(i)
                     meta_fp.write(json.dumps(meta))
 
-                dev_corpus = spacy_convert.convert_abstracts_to_docs(dev_conll_path, dev_pmids_path, vocab_path)
                 nlp_loaded = util.load_model_from_path(epoch_model_path)
-                nwords = sum(len(doc_gold[0]) for doc_gold in dev_corpus)
+                dev_docs = train_corpus.dev_docs(nlp_loaded)
+                dev_docs = list(dev_docs)
+                nwords = sum(len(doc_gold[0]) for doc_gold in dev_docs)
                 start_time = timer()
-                scorer = nlp_loaded.evaluate(dev_corpus)
+                scorer = nlp_loaded.evaluate(dev_docs)
                 end_time = timer()
                 gpu_wps = None
                 cpu_wps = nwords/(end_time-start_time)
@@ -138,10 +119,11 @@ def train_parser_and_tagger(train_conll_path: str,
     with open(os.path.join(model_output_dir, "genia_trained_parser_tagger", "meta.json"), "w") as meta_fp:
         meta_fp.write(json.dumps(meta))
     nlp_loaded = util.load_model_from_path(os.path.join(model_output_dir, "genia_trained_parser_tagger"))
-    nwords = sum(len(doc_gold[0]) for doc_gold in test_corpus)
     start_time = timer()
-    test_corpus = spacy_convert.convert_abstracts_to_docs(test_conll_path, test_pmids_path, vocab_path)
-    scorer = nlp_loaded.evaluate(test_corpus)
+    test_docs = test_corpus.dev_docs(nlp_loaded)
+    test_docs = list(test_docs)
+    nwords = sum(len(doc_gold[0]) for doc_gold in test_docs)
+    scorer = nlp_loaded.evaluate(test_docs)
     end_time = timer()
     gpu_wps = None
     cpu_wps = nwords/(end_time-start_time)
@@ -157,31 +139,25 @@ def train_parser_and_tagger(train_conll_path: str,
         meta_fp.write(json.dumps(meta))
 
     if ontonotes_path:
-        test_docs = list(onto_test_corpus.dev_docs(nlp_loaded))
+        onto_test_docs = list(onto_test_corpus.dev_docs(nlp_loaded))
         print("Retrained ontonotes evaluation")
-        scorer_onto_retrained = nlp_loaded.evaluate(test_docs)
+        scorer_onto_retrained = nlp_loaded.evaluate(onto_test_docs)
         print("Test results:")
         print("UAS:", scorer_onto_retrained.uas)
         print("LAS:", scorer_onto_retrained.las)
         print("Tag %:", scorer_onto_retrained.tags_acc)
         print("Token acc:", scorer_onto_retrained.token_acc)
 
-def main(train_conll_path,
-         dev_conll_path,
-         test_conll_path,
-         train_pmids_path,
-         dev_pmids_path,
-         test_pmids_path,
+def main(train_json_path,
+         dev_json_path,
+         test_json_path,
          vocab_path,
          model_output_dir,
          ontonotes_path):
 
-    train_parser_and_tagger(train_conll_path,
-                            dev_conll_path,
-                            test_conll_path,
-                            train_pmids_path,
-                            dev_pmids_path,
-                            test_pmids_path,
+    train_parser_and_tagger(train_json_path,
+                            dev_json_path,
+                            test_json_path,
                             vocab_path,
                             model_output_dir,
                             ontonotes_path)
@@ -190,33 +166,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        '--train_conll_path',
-        help="Path to the conll formatted training data"
+        '--train_json_path',
+        help="Path to the json formatted training data"
     )
 
     parser.add_argument(
-        '--dev_conll_path',
-        help="Path to the conll formatted dev data"
+        '--dev_json_path',
+        help="Path to the json formatted dev data"
     )
 
     parser.add_argument(
-        '--test_conll_path',
-        help="Path to the conll formatted test data"
-    )
-
-    parser.add_argument(
-        '--train_pmids_path',
-        help="Path to the training pmids, one per conll sentence"
-    )
-
-    parser.add_argument(
-        '--dev_pmids_path',
-        help="Path to the dev pmids, one per conll sentence"
-    )
-
-    parser.add_argument(
-        '--test_pmids_path',
-        help="Path to the test pmids, one per conll sentence"
+        '--test_json_path',
+        help="Path to the json formatted test data"
     )
 
     parser.add_argument(
@@ -232,15 +193,13 @@ if __name__ == "__main__":
     parser.add_argument(
         '--ontonotes_path',
         default=None,
-        help="Path to the ontonotes folder in spacy format")
+        help="Path to the ontonotes folder in spacy format"
+    )
 
     args = parser.parse_args()
-    main(args.train_conll_path,
-         args.dev_conll_path,
-         args.test_conll_path,
-         args.train_pmids_path,
-         args.dev_pmids_path,
-         args.test_pmids_path,
+    main(args.train_json_path,
+         args.dev_json_path,
+         args.test_json_path,
          args.vocab_path,
          args.model_output_dir,
          args.ontonotes_path)
