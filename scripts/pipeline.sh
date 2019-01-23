f77f0e371ad8f6ae771a7321489b9f1af75831cb
Mark Neumann <markng@allenai.org>
2019-01-23 15:25:01 -0800

    recursive copy in pipeline

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -9,15 +9,16 @@ MODEL_NAME=${3:-scispacy_model}
 if [[ -z "${SIZE}" ]]; then
   echo "Usage (run from base SciSpaCy repository):"
   echo "pipeline.sh <size> {small|large} <build_directory> {default=./build} <model name> {default='scispacy_model'}"
+  exit 1
 fi
 
-./scripts/base_model.sh ${SIZE} ${OUT_PATH}/base_model
-./scripts/parser.sh ${OUT_PATH}/base_model ${OUT_PATH}/parser
-./scripts/ner.sh ${OUT_PATH}/parser/best ${OUT_PATH}/ner
+bash ./scripts/base_model.sh ${SIZE} ${OUT_PATH}/base_model
+bash ./scripts/parser.sh ${OUT_PATH}/base_model ${OUT_PATH}/parser
+bash ./scripts/ner.sh ${OUT_PATH}/parser/best ${OUT_PATH}/ner
 
-cp ${OUT_PATH}/ner/best ${OUT_PATH}/${MODEL_NAME}
+cp -r ${OUT_PATH}/ner/best ${OUT_PATH}/${MODEL_NAME}
 
-./scripts/create_model_package.sh ${OUT_PATH}/${MODEL_NAME}
+bash ./scripts/create_model_package.sh ${OUT_PATH}/${MODEL_NAME}
 
 echo "Build output present in ${OUT_PATH}, with the full model serialised to ${OUT_PATH}/${MODEL_NAME}."
 echo "A spacy model package has been created in ./dist."
