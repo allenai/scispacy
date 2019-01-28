558445f7153d0a83336cd8987ccb7650b57af479
Mark Neumann <markng@allenai.org>
2019-01-28 15:22:32 -0800

    add pypi upload instructions, add twine to requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 from setuptools import setup, find_packages
 
 
+"""
+Instructions for creating a release of the scispacy library.
+
+1. Make sure your working directory is clean.
+2. Make sure that you have changed the versions in "scispacy/version.py".
+3. Create the distribution by running "python setup.py sdist" in the root of the repository.
+4. Check you can install the new distribution in a clean environment.
+5. Upload the distribution to pypi by running "twine upload <path to the distribution>".
+   This step will ask you for a username and password - the username is "scispacy" you can
+   get the password from LastPass.
+"""
+
 VERSION = {}
 # version.py defines VERSION and VERSION_SHORT variables.
 # We use exec here to read it so that we don't import scispacy
