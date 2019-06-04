cf6f36fe6407da877d6a86855a5ac7efd8c234c9
danielkingai2 <daniel@allenai.org>
2019-06-03 17:01:08 -0700

    Add test for the type tree being accessible from the linker

--- tests/test_umls_linking.py
+++ tests/test_umls_linking.py
@@ -14,7 +14,7 @@ class TestUmlsLinker(unittest.TestCase):
         super().setUp()
         self.nlp = spacy.load("en_core_web_sm")
 
-        umls_fixture = UmlsKnowledgeBase("tests/fixtures/umls_test_fixture.json")
+        umls_fixture = UmlsKnowledgeBase("tests/fixtures/umls_test_fixture.json", "tests/fixtures/umls_semantic_type_tree.tsv")
         with tempfile.TemporaryDirectory() as dir_name:
             umls_concept_aliases, tfidf_vectorizer, ann_index = create_tfidf_ann_index(dir_name, umls_fixture)
         candidate_generator = CandidateGenerator(ann_index, tfidf_vectorizer, umls_concept_aliases, umls_fixture)
@@ -57,3 +57,7 @@ class TestUmlsLinker(unittest.TestCase):
         assert id_with_score == ("C0000098", 1.0)
         umls_entity = self.linker.umls.cui_to_entity[id_with_score[0]]
         assert umls_entity.concept_id == "C0000098"
+
+    def test_linker_has_types(self):
+        # Just checking that the type tree is accessible from the linker
+        assert len(self.linker.umls.semantic_type_tree.flat_nodes) == 128
