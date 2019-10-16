87d5dc49659a2698b251d5ffaaecbcd7e474589b
danielkingai2 <daniel@allenai.org>
2019-10-16 16:07:10 -0700

    pylint

--- scispacy/custom_sentence_segmenter.py
+++ scispacy/custom_sentence_segmenter.py
@@ -1,9 +1,9 @@
 
-from typing import List, Any
+from typing import List
 
 import pysbd
 
-from spacy.tokens import Doc, Token
+from spacy.tokens import Doc
 
 from scispacy.consts import ABBREVIATIONS # pylint: disable-msg=E0611,E0401
 
@@ -27,18 +27,18 @@ def combined_rule_sentence_segmenter(doc: Doc) -> Doc:
     segmenter = pysbd.Segmenter(language="en", clean=False)
     segments = merge_segments(segmenter.segment(doc.text))
 
-    # pysbd splits raw text into sentences, so we have to do our best to align those 
+    # pysbd splits raw text into sentences, so we have to do our best to align those
     # segments with spacy tokens
     segment_index = 0
     current_segment = segments[segment_index]
     built_up_sentence = ""
-    for i, token in enumerate(doc):
+    for token in enumerate(doc):
         if token.text.replace('\n', '') == '':
             token.is_sent_start = False
         elif len(built_up_sentence) >= len(current_segment):
             token.is_sent_start = True
 
-            # handle the rare (impossible?) case where spacy tokenizes over a sentence boundary that 
+            # handle the rare (impossible?) case where spacy tokenizes over a sentence boundary that
             # pysbd finds
             built_up_sentence = ' '*int(len(built_up_sentence) - len(current_segment))
             built_up_sentence = token.string
