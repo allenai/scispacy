254ff4ec84a7f5b3f3feb4546f8a409b20428ea9
Nipun Sadvilkar <nipunsadvilkar@gmail.com>
2019-10-23 20:21:38 +0530

    flake8

--- scispacy/custom_sentence_segmenter.py
+++ scispacy/custom_sentence_segmenter.py
@@ -17,7 +17,7 @@ def combined_rule_sentence_segmenter(doc: Doc) -> Doc:
     char_spans = [
         doc.char_span(sent_span.start, sent_span.end)
         for sent_span in sents_char_spans
-        ]
+    ]
     start_token_ids = [span[0].idx for span in char_spans if span is not None]
     for token in doc:
         token.is_sent_start = True if token.idx in start_token_ids else False
