b5d946401ef9fc2d8b73f7b4c6c0e4ed91b711d9
Nipun Sadvilkar <nipunsadvilkar@gmail.com>
2019-11-03 13:07:27 +0530

    ✅ 👌  Update tests & comments
    testing sent_split within token & reviews fixes

--- scispacy/custom_sentence_segmenter.py
+++ scispacy/custom_sentence_segmenter.py
@@ -22,16 +22,33 @@ def combined_rule_sentence_segmenter(doc: Doc) -> Doc:
         doc.char_span(sent_span.start, sent_span.end)
         for sent_span in sents_char_spans
     ]
-    start_token_ids = [span[0].idx for span in char_spans if span is not None]
+    start_token_char_offsets = [span[0].idx for span in char_spans if span is not None]
     for token in doc:
         prev_token = token.nbor(-1) if token.i != 0 else None
-        if token.idx in start_token_ids:
+        if token.idx in start_token_char_offsets:
             if prev_token and prev_token.text in ABBREVIATIONS:
                 token.is_sent_start = False
             else:
                 token.is_sent_start = True
+        # check if previous token contains more than 2 newline chars
         elif prev_token and prev_token.i != 0 and prev_token.text.count('\n') >= 2:
             token.is_sent_start = True
         else:
             token.is_sent_start = False
     return doc
+
+
+if __name__ == "__main__":
+    import spacy
+    from scispacy.custom_tokenizer import combined_rule_tokenizer, combined_rule_prefixes
+
+    nlp = spacy.blank('en')
+    nlp.tokenizer = combined_rule_tokenizer(nlp)
+    nlp.add_pipe(combined_rule_sentence_segmenter, first=True)
+    # text = "When the tree is simply a chain, both Eqs. 2–8 and Eqs. 9–14 reduce to the standard LSTM transitions, Eqs. 1."
+    # text = "First sentence with char in the middle.adjacent sentence to it."
+    text = 'How about tomorrow?We can meet at eden garden.'
+    doc = nlp(text)
+    for sent_id, sent in enumerate(doc.sents, start=1):
+        print(sent_id, repr(sent.text), sep='\t')
+    # expected_sents = ['\n\n2 Long Short-Term Memory Networks\n\n\n\n', '2.1 Overview\n\n', 'Recurrent neural networks (RNNs) are able to pr...put sequences of arbitrary length via the recursive application of a transition function on a hidden state vector ht.']
