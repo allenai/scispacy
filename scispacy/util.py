eb6bb82c236586b7aa55f391bc6350c779c62c06
Mark Neumann <markn@allenai.org>
2019-11-06 09:35:56 +0800

    help fix build

--- scispacy/util.py
+++ scispacy/util.py
@@ -2,7 +2,7 @@ import spacy
 from spacy.language import Language
 from spacy.tokens import Doc
 
-from scispacy.custom_sentence_segmenter import combined_rule_sentence_segmenter # pylint: disable-msg=E0611,E0401
+from scispacy.custom_sentence_segmenter import pysbd_sentencizer # pylint: disable-msg=E0611,E0401
 from scispacy.custom_tokenizer import combined_rule_tokenizer # pylint: disable-msg=E0611,E0401
 
 def save_model(nlp: Language, output_path: str):
@@ -11,7 +11,7 @@ def save_model(nlp: Language, output_path: str):
 def create_combined_rule_model() -> Language:
     nlp = spacy.load('en_core_web_sm')
     nlp.tokenizer = combined_rule_tokenizer(nlp)
-    nlp.add_pipe(combined_rule_sentence_segmenter, first=True)
+    nlp.add_pipe(pysbd_sentencizer, first=True)
     return nlp
 
 
