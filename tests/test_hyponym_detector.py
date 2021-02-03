5217bc9721504906e621990b250c5eb5011dcc03
Daniel King <daniel@allenai.org>
2021-02-02 18:51:19 -0800

    Fix hyponym detector tests

--- tests/test_hyponym_detector.py
+++ tests/test_hyponym_detector.py
@@ -6,29 +6,31 @@ from scispacy.hyponym_detector import HyponymDetector
 
 
 class TestHyponymDetector(unittest.TestCase):
-
     def setUp(self):
         super().setUp()
         self.nlp = spacy.load("en_core_sci_sm")
         self.detector = HyponymDetector(self.nlp, extended=True)
-        self.nlp.add_pipe(self.detector, last=True)
+
+        nlp_sm = spacy.load("en_core_web_sm")
+        self.nlp.add_pipe("attribute_ruler", source=nlp_sm, after="tagger")
+        self.nlp.add_pipe("lemmatizer", source=nlp_sm, after="attribute_ruler")
+        self.nlp.add_pipe("hyponym_detector", config={"extended": True}, last=True)
 
     def test_sentences(self):
-        text = ("Recognizing that the preferred habitats for the species "
-                "are in the valleys, systematic planting of keystone plant "
-                "species such as fig trees (Ficus) creates the best microhabitats.")
+        text = (
+            "Recognizing that the preferred habitats for the species "
+            "are in the valleys, systematic planting of keystone plant "
+            "species such as fig trees (Ficus) creates the best microhabitats."
+        )
         doc = self.nlp(text)
         fig_trees = doc[21:23]
         keystone_plant_species = doc[16:19]
-        assert doc._.hearst_patterns == [('such_as', keystone_plant_species, fig_trees)]
+        assert doc._.hearst_patterns == [("such_as", keystone_plant_species, fig_trees)]
 
         doc = self.nlp("SARS, or other coronaviruses, are bad.")
         assert doc._.hearst_patterns == [("other", doc[4:5], doc[0:1])]
         doc = self.nlp("Coronaviruses, including SARS and MERS, are bad.")
-        assert doc._.hearst_patterns == [
-            ("include", doc[0:1], doc[3:4]),
-            ("include", doc[0:1], doc[5:6])
-        ]
+        assert doc._.hearst_patterns == [("include", doc[0:1], doc[3:4]), ("include", doc[0:1], doc[5:6])]
 
     def test_find_noun_compound_head(self):
 
