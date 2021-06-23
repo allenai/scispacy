82698445dc89bce1a620bd65ec372abe76c9bb6a
MichalMalyska <malyskamichal@gmail.com>
2021-06-23 12:24:03 -0400

    Add a test for pipeline serializability

--- tests/custom_tests/test_all_model.py
+++ tests/custom_tests/test_all_model.py
@@ -39,3 +39,8 @@ def test_custom_segmentation(combined_all_model_fixture):
     assert doc.has_annotation("DEP")
     assert doc[0].dep_ == "ROOT"
     assert doc[0].tag_ == "NN"
+
+def test_full_pipe_serializable(combined_all_model_fixture):
+    text = "Induction of cytokine expression in leukocytes (CEIL) by binding of thrombin-stimulated platelets. BACKGROUND: Activated platelets tether and activate myeloid leukocytes."
+    doc = [doc for doc in combined_all_model_fixture.pipe([text, text], n_process = 2)][0]
+    assert True
