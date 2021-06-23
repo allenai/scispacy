2874f414f121899174a63f149e22bfe0d55438ca
MichalMalyska <malyskamichal@gmail.com>
2021-06-23 12:48:09 -0400

    Add more explicit testing and comments

--- tests/custom_tests/test_all_model.py
+++ tests/custom_tests/test_all_model.py
@@ -43,4 +43,5 @@ def test_custom_segmentation(combined_all_model_fixture):
 def test_full_pipe_serializable(combined_all_model_fixture):
     text = "Induction of cytokine expression in leukocytes (CEIL) by binding of thrombin-stimulated platelets. BACKGROUND: Activated platelets tether and activate myeloid leukocytes."
     doc = [doc for doc in combined_all_model_fixture.pipe([text, text], n_process = 2)][0]
-    assert True
+    # If we got here this means that both model is serializable and there is an abbreviation that would break if it wasn't
+    assert len(doc._.abbreviations) > 0
