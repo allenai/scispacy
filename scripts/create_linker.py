767260c99813b3d80cb51383976f8324937c77b9
nachollorca <madwayesp@gmail.com>
2023-05-07 15:02:54 +0200

    Add custom n-gram range option

--- scripts/create_linker.py
+++ scripts/create_linker.py
@@ -5,11 +5,11 @@ from scispacy.candidate_generation import create_tfidf_ann_index
 from scispacy.linking_utils import KnowledgeBase
 
 
-def main(kb_path: str, output_path: str):
+def main(kb_path: str, output_path: str, n_grams: Tuple[int,int] = (3, 3)):
 
     os.makedirs(output_path, exist_ok=True)
     kb = KnowledgeBase(kb_path)
-    create_tfidf_ann_index(output_path, kb)
+    create_tfidf_ann_index(output_path, kb, n_grams)
 
 
 if __name__ == "__main__":
@@ -22,6 +22,11 @@ if __name__ == "__main__":
         '--output_path',
         help="Path to the output directory."
     )
+    parser.add_argument(
+        '--n_grams',
+        type=int,
+        help="Use n grams to build the index"
+    )
 
     args = parser.parse_args()
-    main(args.kb_path, args.output_path)
+    main(args.kb_path, args.output_path, args.n_grams)
